f2aeea2620c428c1016d561d65e0e3b333f585c8
puggert@bao <puggy@gaming.com>
2025-05-29 17:37:38 -0400

    pug's second commit

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,249 @@
 use rust_on_rails::prelude::*;
+//yello
 
 use std::collections::HashMap;
 use crossterm::{execute, terminal::ClearType};
 use std::io::{stdout, Write};
 use std::time::Instant;
+use dyn_clone::DynClone;
+use dyn_clone::clone_trait_object;
+use rand::Rng;
 
+//pub struct Shoot{
+	//execute(&self, board: &mut HashMap<etc>, current_coord: Coord)
+//}
+//pub struct Move{}
+//pub struct Remove{
+//}
+
+//pub trait Action {
+	//execute(&self, board: &mut HashMap<etc>, current_coord: Coord)
+//}
+pub enum Action{
+	Shoot,
+	Move(Coord),
+	Remove,
+}
+
+/*#[derive(Clone, Debug)]
+pub struct UpBullet{}
+impl ShipTrait for UpBullet {
+	fn draw(&self, ctx: &mut Context, x: u8, y: u8) {
+		ctx.draw(CanvasItem::Shape(
+			Area((SIZE * x as u32, SIZE * y as u32), None),
+			Shape::Ellipse(0, (SIZE, SIZE)),
+			"0000FF", 255
+		));
+	}
+	fn get_next_move(&mut self, current_position: Coord) -> Action {
+		match current_position.1.checked_sub(1) {
+			Some(whatever) => {
+				Action::Move((current_position.0, current_position.1-1))
+			},
+			None => {
+				Action::Remove
+			},
+		}
+	}
+	fn is_enemy(&self) -> bool {
+		false
+	}
+	fn get_id(&self) -> u32 {}
+}*/
+//#[derive(Clone, Debug)]
+/*pub struct DownBullet{}
+impl ShipTrait for DownBullet {
+	fn draw(&self, ctx: &mut Context, x: u8, y: u8) {
+		ctx.draw(CanvasItem::Shape(
+			Area((SIZE * x as u32, SIZE * y as u32), None),
+			Shape::Ellipse(0, (SIZE, SIZE)),
+			"ffD700", 255
+		));
+	}
+	fn get_next_move(&mut self, current_position: Coord) -> Action {
+		match current_position.1.checked_add(1) {
+			Some(whatever) => {
+				Action::Move((current_position.0, current_position.1+1))
+			},
+			None => {
+				Action::Remove
+			},
+		}
+	}
+	fn is_enemy(&self) -> bool {
+		false
+	}
+	fn get_id(&self) -> u32 {}
+}*/
+
+#[derive(Debug, Clone)]
+pub struct Fly{shoot_next: bool, id: u32}
+impl ShipTrait for Fly {
+	fn draw(&self, ctx: &mut Context, x: u8, y: u8) {
+		ctx.draw(CanvasItem::Shape(
+			Area((SIZE * x as u32, SIZE * y as u32), None),
+			Shape::Ellipse(0, (SIZE, SIZE)),
+			"008000", 255
+		));
+	}
+	fn get_next_move(&mut self, current_position: Coord) -> Action {
+		let switch = match self.shoot_next {
+			true => {
+				match current_position.1.checked_add(1) {
+					Some(whatever) => {
+						Action::Move((current_position.0+1, current_position.1))
+					},
+					None => {
+						Action::Remove
+					},
+				}
+			},
+			false => {
+				Action::Shoot
+			},
+		};
+		self.shoot_next = !self.shoot_next;
+		return switch;
+	}
+	fn is_enemy(&self) -> bool {
+		true
+	}
+	fn get_id(&self) -> u32 {
+		rand::rng().random()
+	}
+}
+	fn new() -> Fly {
+		Fly{shoot_next: true, id: rand::rng().random()}
+	}
+
+pub trait ShipTrait: std::fmt::Debug + DynClone {
+	fn draw(&self, ctx: &mut Context, x: u8, y: u8);
+	fn get_next_move(&mut self, current_position: Coord) -> Action;
+	fn is_enemy(&self) -> bool;
+	fn get_id(&self) -> u32;
+}
+
+
+
+clone_trait_object!(ShipTrait);
+	//ships: Vec<Box<dyn ShipTrait>> = vec![Box::new(DownBullet{})];
 pub type Coord = (u8, u8);
 const BSIZE: u8 = 20;
 const CSIZE: u8 = 19;
 const SIZE: u32 = 30;
-//COMPLETED: create an enum Ship with variants DownBullet, UpBullet.
-//COMPLETED: give it a method called get_char that returns the character based on its variant.
-//TODO: switch out the char in board to Ship and use the draw method to get the character needed to print.
-#[derive(Clone, Debug)]
-enum Ship {
-	DownBullet,
-	UpBullet,
-}
 
+#[derive(Debug)]
 struct MyApp {
 	player_position: Coord,
-	board: HashMap<Coord, Ship>,
+	board: HashMap<Coord, Box<dyn ShipTrait>>, //Box<Ship> Trait
 	last_tick: Instant,
+	fly: HashMap<Coord, Box<dyn ShipTrait>>,
 }
 
 impl App for MyApp {
 	async fn new(ctx: &mut Context) -> Self {
 		MyApp{
-        	board: HashMap::from([((10, 5), Ship::DownBullet)]),
+        	board: HashMap::from([(
+				(8, 10),
+				Box::new(Fly::new()) as Box<dyn ShipTrait>
+			)]),
 			player_position: (10, 19),
 			last_tick: Instant::now(),
+			fly: HashMap::from([
+				((2, 5), Box::new(Fly::new()) as Box<dyn ShipTrait>),
+				((4, 5), Box::new(Fly::new()) as Box<dyn ShipTrait>),
+				((6, 5), Box::new(Fly::new()) as Box<dyn ShipTrait>),
+				((8, 5), Box::new(Fly::new()) as Box<dyn ShipTrait>),
+				((10, 5), Box::new(Fly::new()) as Box<dyn ShipTrait>),
+				((12, 5), Box::new(Fly::new()) as Box<dyn ShipTrait>),
+				((14, 5), Box::new(Fly::new()) as Box<dyn ShipTrait>),
+			]),
 		}
 	}
-
 	async fn draw(&mut self, ctx: &mut Context) {
 		if self.last_tick.elapsed().as_millis() > 1000 {
-			Ship.next_move(ctx, self.board, self.player_position);
 			self.last_tick = Instant::now();
+			self.move_ships();
 		}
-        self.print(ctx)
+		self.print(ctx)
 	}
 	async fn on_click(&mut self, ctx: &mut Context) {}
 	async fn on_move(&mut self, ctx: &mut Context) {}
+	//matches on a String type with multiple key bindings that can act as user movement.
+	//the movement can only be enabled if it is within the boundries of the board.
+	//the shoot key binding inserts an a bullet above the player position when pressed
 	async fn on_press(&mut self, ctx: &mut Context, t: String) {
-		//COMPLETED: make it so if you hit the character e you'll shoot
 			match t.as_str() {
 				"a" if self.player_position.0 > 0 => self.player_position = (self.player_position.0-1, self.player_position.1),
 				"d" if self.player_position.0 < BSIZE - 1 => self.player_position = (self.player_position.0+1, self.player_position.1),
 				"w" if self.player_position.1 > BSIZE - 3 => self.player_position = (self.player_position.0, self.player_position.1-1),
 				"s" if self.player_position.1 < CSIZE => self.player_position = (self.player_position.0, self.player_position.1+1),
-				"e" => {
-					let above = self.board.insert((self.player_position.0, self.player_position.1-1), Ship::UpBullet);
-				}
+				//"e" => {
+					//let above = self.board.insert((self.player_position.0, self.player_position.1-1), Box::new(UpBullet{}));
+				//}
 				_ => {}
 			};
 			println!("{}", t);
 		}
 }
-impl Ship {
-	fn get_char(self) -> char  {
-		match self {
-			Ship::UpBullet => '|',
-			Ship::DownBullet => '/',
-		}
-	}
+impl MyApp {
+	///move_ships loops through all the ships in the board
+	///asks each ship for its next position after passing its current position
+	///checks the next position for a collision and moves or removes the ship
+	//respawn if hit the edge
+	//flying patterns
+	//change the enum Action to an Action trait
 
-	fn draw(&self, ctx: &mut Context, x: u8, y: u8) {
-		match self {
-			Ship::UpBullet => ctx.draw(CanvasItem::Shape(
-				Area((SIZE * x as u32, SIZE * y as u32), None),
-				Shape::Ellipse(0, (SIZE, SIZE)),
-				"0000FF", 255
-			)),
-			Ship::DownBullet => ctx.draw(CanvasItem::Shape(
-				Area((SIZE * x as u32, SIZE * y as u32), None),
-				Shape::Ellipse(0, (SIZE, SIZE)),
-				"ffD700", 255
-			)),
-			_ => {}
-		};
-	}
-	fn next_move(mut self, ctx: &Context, board: HashMap<Coord, Ship>, player_position: Coord) {
-		let occupied: Vec<Coord> = board.keys()
+	fn move_ships(&mut self) {
+		if self.board.iter().any(|(_, ship)| ship.is_enemy()) {
+				println!("they're still alive");
+			} else {
+				println!("they're all dead");
+				self.board.extend(self.fly.clone());
+			}
+		let occupied: Vec<Coord> = self.board.keys()
 			.copied()
 			.collect();
-			print!("{:?}", board);
 		for coord in occupied {
-			match board.get(&coord) {
-				Some(borrowed_char) => {
-					let ship = borrowed_char.clone();
-					let next_coord: Coord = match ship {
-						Ship::UpBullet => (coord.0, coord.1-1),
-						Ship::DownBullet => (coord.0, coord.1+1),
-						_ => (coord.0, coord.0),
-					};
-					self.remove_char(&coord);
-					if coord == player_position {
-						panic!("HOUSTON WE HAVE A PROBLEM")
-					}
-					match board.get(&next_coord) {
-						Some(_) => {
-							self.remove_char(&next_coord);
+			//matches on the optional Ship at the coordinate to check that it hasn't been destroyed.
+			match self.board.remove(&coord) {
+				Some(mut ship) => {
+					match ship.get_next_move(coord) {
+						Action::Shoot => {
+							//let below = self.board.insert((coord.0, coord.1+1), Box::new(DownBullet{}));
+							self.add_char(coord, ship);
 						},
-						None => {
-							self.add_char(next_coord, ship);
-						}
-					};
+						Action::Move(next_coord) => {
+							if next_coord == self.player_position {
+								panic!("HOUSTON WE HAVE A PROBLEM")
+							};
+							//Checks for a ship at the next coord and removes it if anything (_) is there, if nothing is there (None) move the Ship to the next coord.
+							match self.board.get(&(next_coord)) {
+								Some(_) => {
+									self.remove_char(&next_coord);
+								},
+								None => {
+									self.add_char(next_coord, ship);
+								}
+							}
+						},
+						Action::Remove => {},
+					}
 				}
 				None => {}
 			}
 		}
 	}
-}
-impl MyApp {
-    fn add_char(&mut self, coord: Coord, ship: Ship) {
+	//method of GameState that inserts a tuple of u8s key (Coord) and a value of char (ship). when called upon you must insert that same key and value types.
+    fn add_char(&mut self, coord: Coord, ship: Box<dyn ShipTrait>) {
         self.board.insert(coord, ship);
 	}
+	//method of GameState that removes a value of the hashmap based on the coordinates given as a key.
 	fn remove_char(&mut self, coord: &Coord) {
 		self.board.remove(coord);
 	}
+	//first creates the backround for our board. then, it creates a for loop and another for loop inside of that one to create our 20x20 board. afterwards, x and y variables from the for loop our used to act a coordinate placeholder stored inside coord.
+	//TODO: finish this later
 	fn print(&self, ctx: &mut Context) {
 		ctx.clear("000000");
         for y in 0..BSIZE {
@@ -141,7 +266,7 @@ impl MyApp {
 									"ff00ff", 255
 								));
 							},
-							Some(ship) => {}
+							Some(ship) => ship.draw(ctx, x, y)
 						}
 					}
 				}
@@ -150,7 +275,7 @@ impl MyApp {
     }
 }
 
-
+//TODO: insert a bunch of Flies onto the board and make it so when you destory all of them they all respawn
 create_entry_points!(MyApp);
 
 fn main() {
